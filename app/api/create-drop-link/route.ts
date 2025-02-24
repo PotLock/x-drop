560f2048c4884c99bcbd472f167478ae2f0ed7cf
kurodenjiro <kurodenjiro1@gmail.com>
2025-02-25 02:26:57 +0700

    update

--- app/api/create-drop-link/route.ts
+++ app/api/create-drop-link/route.ts
@@ -30,7 +30,30 @@ export async function POST(req: NextRequest) {
         const dropKeyPair = KeyPair.fromString(dropSecret as any);
 
 
-        return NextResponse.json({ dropLink:"" }, { status: 200 });
+        const dropId = await contractCall({
+            contractId,
+            methodName: 'add_drop',
+            args: {
+                target: 1,
+                amount: amount.toString(), // Sats
+                funder: btcPublicKey,
+                path: MPC_PATH,
+            },
+        });
+        // how to know drop id after created?
+        await contractCall({
+            contractId,
+            methodName: 'add_drop_key',
+            args: {
+                drop_id: dropId,
+                key: dropKeyPair.getPublicKey().toString(),
+            },
+        });
+        console.log('Drop key added', dropKeyPair);
+        const dropKeyPairBase64 = Buffer.from(dropSecret).toString('base64');
+
+        const dropLink = `${NEXT_PUBLIC_URL}/claim/${dropId}?key=${dropKeyPairBase64}`;
+        return NextResponse.json({ dropLink: dropLink }, { status: 200 });
     } catch (error) {
         console.error('Error creating drop link:', error);
         return NextResponse.json({ error: 'Failed to create drop link' }, { status: 500 });
